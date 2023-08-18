89f7e12e64f784d68c544586a33173e4678e5a1c
SmoonliX <ananas1.19@mail.ru>
2023-08-18 15:11:24 +0300

    Переводчик

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,29 @@
 use std::collections::HashMap;
-use std::io;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 fn clear_screen() {
     print!("{}[2J{}[1;1H", 27 as char, 27 as char);
 }
 
-fn create_translation_dicts() -> (HashMap<&'static str, &'static str>, HashMap<&'static str, &'static str>) {
-    let mut dict_ru_to_en = HashMap::new();
-    dict_ru_to_en.insert("привет", "hello");
-    dict_ru_to_en.insert("мир", "world");
-    dict_ru_to_en.insert("мама", "mother");
-    dict_ru_to_en.insert("папа", "dad");
-    dict_ru_to_en.insert("работа", "job");
-    dict_ru_to_en.insert("практика", "practice");
-    dict_ru_to_en.insert("первый", "first");
-    dict_ru_to_en.insert("телефон", "telephone");
-    dict_ru_to_en.insert("фонарик", "flashlight");
-    dict_ru_to_en.insert("программирование", "programming");
-    dict_ru_to_en.insert("логика", "logics");
-    dict_ru_to_en.insert("письмо", "letter");
-
-    let mut dict_en_to_ru = HashMap::new();
-    dict_en_to_ru.insert("hello", "привет");
-    dict_en_to_ru.insert("world", "мир");
-    dict_en_to_ru.insert("mother", "мама");
-    dict_en_to_ru.insert("dad", "папа");
-    dict_en_to_ru.insert("job", "работа");
-    dict_en_to_ru.insert("practice", "практика");
-    dict_ru_to_en.insert("first", "первый");
-    dict_ru_to_en.insert("telephone", "телефон");
-    dict_ru_to_en.insert("flashlight", "фонарик");
-    dict_ru_to_en.insert("programming", "программирование");
-    dict_ru_to_en.insert("logics", "логика");
-    dict_ru_to_en.insert("letter", "письмо");
-
-    (dict_ru_to_en, dict_en_to_ru)
+fn load_dictionary(file_path: &str) -> HashMap<String, String> {
+    let file = File::open(file_path).expect("Ошибка открытия файла");
+    let reader = BufReader::new(file);
+    
+    let mut dict = HashMap::new();
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            let parts: Vec<_> = line.splitn(2, '=').collect();
+            if parts.len() == 2 {
+                dict.insert(parts[0].trim().to_string(), parts[1].trim().to_string());
+            }
+        }
+    }
+    
+    dict
 }
 
-fn translate_text(input_text: &str, dict: &HashMap<&str, &str>) -> String {
+fn translate_text(input_text: &str, dict: &HashMap<String, String>) -> String {
     let mut result = String::new();
     for word in input_text.split_whitespace() {
         if let Some(translation) = dict.get(word) {
@@ -51,9 +37,9 @@ fn translate_text(input_text: &str, dict: &HashMap<&str, &str>) -> String {
 }
 
 fn main() {
-    let (dict_ru_to_en, dict_en_to_ru) = create_translation_dicts();
+    let dict = load_dictionary("dictionary.txt");
 
-    clear_screen(); // Очистка экрана
+    clear_screen();
 
     let mut input_text = String::new();
     println!("Введите текст для перевода:");
@@ -64,14 +50,17 @@ fn main() {
     io::stdin().read_line(&mut choice).expect("Ошибка чтения строки");
 
     let translated_text = match choice.trim() {
-        "1" => translate_text(input_text.trim(), &dict_ru_to_en),
-        "2" => translate_text(input_text.trim(), &dict_en_to_ru),
+        "1" => translate_text(input_text.trim(), &dict),
+        "2" => {
+            let reversed_dict: HashMap<String, String> = dict.iter().map(|(k, v)| (v.clone(), k.clone())).collect();
+            translate_text(input_text.trim(), &reversed_dict)
+        }
         _ => {
             println!("Некорректный выбор. Текст останется без изменений.");
             return;
         }
     };
 
-    clear_screen(); // Очистка экрана
+    clear_screen();
     println!("Перевод: {}", translated_text);
 }
